1e5198935e2bc5f765c711552322bf99c3010619
sophiamziou <sofiamziou@gmail.com>
2023-01-10 15:30:42 +0100

    exercise done

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: "AppTitle",
+    data(){
+        return {
+            message: "La mia prima app con Vite!"
+        }
+    }
+}
+</script>
+
+<template>
+  <div class="title">
+    {{ message }}
+  </div>
+</template>
+
+<style scoped>
+    .title{
+        padding: 50px;
+        background-color: antiquewhite;
+        color: rgb(22, 137, 183);
+        font-size: 50px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
